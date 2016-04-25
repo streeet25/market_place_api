2d3c2ae7f176753d27eba616212ab39bc8ec0c81
streeet25 <streeet25@gmail.com>
2016-04-25 19:37:38 +0300

    Adds the user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -19,4 +19,46 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     	expect(response).to have_http_status(:ok)
   	end
   end
+
+  describe "POST #create" do
+
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it "is should return http status 201" do
+    	expect(response).to have_http_status(201)
+  	end
+    end
+
+    context "when is not created" do
+      before(:each) do
+        #notice I'm not including the email
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it "is should return http status 422" do
+    	expect(response).to have_http_status(422)
+  	end
+    end
+  end
 end
