98c5cb00193dd4b7b355ba7be900179d0d5ab779
streeet25 <streeet25@gmail.com>
2016-04-28 18:08:25 +0300

    Updates the total calculation for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -13,18 +13,25 @@ describe Order do
   it { should have_many(:placements) }
   it { should have_many(:products).through(:placements) }
 
-  describe '#set_total!' do
+    describe '#set_total!' do
     before(:each) do
       product_1 = FactoryGirl.create :product, price: 100
       product_2 = FactoryGirl.create :product, price: 85
 
-      @order = FactoryGirl.build :order, product_ids: [product_1.id, product_2.id]
+      placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
+      placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
+
+      @order = FactoryGirl.build :order
+
+      @order.placements << placement_1
+      @order.placements << placement_2
     end
 
     it "returns the total amount to pay for the products" do
-      expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
+      expect{@order.set_total!}.to change{@order.total.to_f}.from(0).to(1575)
     end
   end
+
 
   describe "#build_placements_with_product_ids_and_quantities" do
     before(:each) do
